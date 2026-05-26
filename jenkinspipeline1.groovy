71ddef8828d7ce5ba61ecfcaa6f4952f78b55bef
insightedgepune <insightedgepune@gmail.com>
2026-05-26 17:12:07 +0530

    Add files via upload

---
+++ jenkinspipeline1.groovy
@@ -0,0 +1,67 @@
+pipeline {
+
+    agent {
+        label 'ansible-node'
+    }
+
+    environment {
+
+        DEV_REPO = 'https://github.com/company/dev-repo.git'
+
+        DEVOPS_REPO = 'https://github.com/company/devops-repo.git'
+    }
+
+    stages {
+
+        stage('Checkout Dev Repo') {
+
+            steps {
+
+                git branch: 'main',
+                credentialsId: 'git-token',
+                url: "${DEV_REPO}"
+            }
+        }
+
+        stage('Checkout DevOps Repo') {
+
+            steps {
+
+                dir('devops') {
+
+                    git branch: 'main',
+                    credentialsId: 'git-token',
+                    url: "${DEVOPS_REPO}"
+                }
+            }
+        }
+
+        stage('Deploy Flask App') {
+
+            steps {
+
+                sshagent(['target-server-key']) {
+
+                    sh '''
+                    ansible-playbook \
+                    -i devops/inventory \
+                    devops/deploy_flask.yml
+                    '''
+                }
+            }
+        }
+    }
+
+    post {
+
+        success {
+
+            echo 'Deployment Successful'
+        }
+
+        failure {
+
+            echo 'Deployment Failed'
+        }
+    }
+}
